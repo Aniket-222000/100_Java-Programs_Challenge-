72d81ad197770f87f90a7fe276a37d9b4a1df990
Aniket-222000 <chakkarwaraniket2@gmail.com>
2023-03-22 10:39:22 +0530

    second program

---
+++ PrintInteger.java
@@ -0,0 +1,12 @@
+public class PrintInteger 
+{
+public static void main(String[] args) {
+    int n = 10;
+
+    for (int i = 0 ; i < n ; i ++)
+    {
+        System.out.println(i);
+    }
+}
+    
+}
